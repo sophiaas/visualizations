60fa57be8cd630def9de209bcb8e2903c9b0e053
Sophia <sophia.sanborn@gmail.com>
2021-02-25 11:53:55 -0800

    share range option

--- svis/image.py
+++ svis/image.py
@@ -2,7 +2,7 @@ from mpl_toolkits.axes_grid1 import ImageGrid
 import matplotlib.pyplot as plt
 
 
-def image_grid(data, shape=(10,10), figsize=(10,10), cmap='viridis'):
+def image_grid(data, shape=(10,10), figsize=(10,10), cmap='viridis', share_range=True):
 
     fig = plt.figure(figsize=figsize)
     grid = ImageGrid(fig, 111,  # similar to subplot(111)
@@ -10,9 +10,16 @@ def image_grid(data, shape=(10,10), figsize=(10,10), cmap='viridis'):
                      axes_pad=0.1,  # pad between axes in inch.
                      )
 
+    if share_range:
+        vmin = data.min()
+        vmax = data.max()
+        
     for ax, im in zip(grid, data):
         # Iterating over the grid returns the Axes.
-        ax.imshow(im)
+        if share_range:
+            ax.imshow(im, vmin=vmin, vmax=vmax)
+        else:
+            ax.imshow(im)
         ax.set_axis_off()
 
     plt.show()
